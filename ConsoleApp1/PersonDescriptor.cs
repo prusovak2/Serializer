e9b5d721a026bceebed20547056c5156667240c0
prusovak2 <prusova.k2@seznam.cz>
2020-04-18 22:18:43 +0200

    Add project files.

---
+++ ConsoleApp1/PersonDescriptor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnreflectedSerializer
+{
+    class PersonDescriptor
+    {
+        private Person person { get; set; }
+        public string GetFirstNameFieldName => "FirstName";
+        public string GetFirstName => this.person.FirstName;
+        public string GetLastNameFieldName => "LastName";
+        public string GetLasttName => this.person.LastName;
+        public string GetHomeAdressFieldName => "HomeAddress";
+        public AddressDescriptor HomeAddress { get; set; }
+        public string GetWorkAdressFieldName => "WorkAddress";
+        public AddressDescriptor WorkAddress { get; set; }
+        public string GetCitizenOfFieldName => "CitizenOf";
+        public CountryDescriptor CitizenOf { get; set; }
+        public string GetMobilePhoneFieldName => "MobilePhone";
+        public PhoneNumberDescriptor MobilePhone { get; set; }
+
+        public int NumOfFields = 6;
+
+        public PersonDescriptor()
+        {
+            this.HomeAddress = new AddressDescriptor();
+            this.WorkAddress = new AddressDescriptor();
+            this.CitizenOf = new CountryDescriptor();
+            this.MobilePhone = new PhoneNumberDescriptor();
+        }
+        public void FillValues(Person person)
+        {
+            this.person = person;
+            this.HomeAddress.FillValues(person.HomeAddress);
+            this.WorkAddress.FillValues(person.WorkAddress);
+            this.CitizenOf.FillValues(person.CitizenOf);
+            this.MobilePhone.FillValues(person.MobilePhone);
+        }
+
+    }
+    class AddressDescriptor
+    {
+        private Address address { get; set; }
+        public string StreetFieldName => "Street";
+        public string GetStreet => this.address.Street;
+        public string CityFieldName => "City";
+        public string GetCity => this.address.City;
+
+        public int NumOfFields = 2;
+
+        public void FillValues(Address adress)
+        {
+            this.address = address;
+        }
+    }
+    class CountryDescriptor
+    {
+        private Country country { get; set; }
+        public string GetNameFieldName => "Name";
+        public string GetName => this.country.Name;
+        public string GetAreaCodeFieldName => "AreaCode";
+        public string GetAreaCode => this.country.AreaCode.ToString();
+
+        public int NumOfFileds = 2;
+        public void FillValues(Country country)
+        {
+            this.country = country;
+        }
+    }
+    class PhoneNumberDescriptor
+    {
+        private PhoneNumber phoneNumber { get; set; }
+        public string GetCountryFieldName => "Country";
+        public CountryDescriptor Country { get; set; }
+        public string GetNumberFieldName => "Number";
+        public string GetNmber => this.phoneNumber.Number.ToString();
+
+        public int NumOfFilds = 2;
+        public PhoneNumberDescriptor()
+        {
+            this.Country = new CountryDescriptor();
+        }
+        public void FillValues(PhoneNumber phone)
+        {
+            this.phoneNumber = phone;
+            this.Country.FillValues(phone.Country);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UnreflectedSerializer
+{
+	public delegate string GetFieldName();
+	public delegate string GetValue();
+	public delegate void FillValues<T>(T instance);
+
+	public class RootDescriptor<T>
+	{
+		public FillValues<T> fillValues;
+		public List<GetFieldName> getFieldNames = new List<GetFieldName>();
+		public List<GetValue> getValues = new List<GetValue>();
+
+		public void Serialize(TextWriter writer, T instance)
+		{
+			
+		}
+	}
+
+	class Address
+	{
+		public string Street { get; set; }
+		public string City { get; set; }
+	}
+
+	class Country
+	{
+		public string Name { get; set; }
+		public int AreaCode { get; set; }
+	}
+
+	class PhoneNumber
+	{
+		public Country Country { get; set; }
+		public int Number { get; set; }
+	}
+
+	class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Address HomeAddress { get; set; }
+		public Address WorkAddress { get; set; }
+		public Country CitizenOf { get; set; }
+		public PhoneNumber MobilePhone { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RootDescriptor<Person> rootDesc = GetPersonDescriptor();
+
+			var czechRepublic = new Country { Name = "Czech Republic", AreaCode = 420 };
+			var person = new Person
+			{
+				FirstName = "Pavel",
+				LastName = "Jezek",
+				HomeAddress = new Address { Street = "Patkova", City = "Prague" },
+				WorkAddress = new Address { Street = "Malostranske namesti", City = "Prague" },
+				CitizenOf = czechRepublic,
+				MobilePhone = new PhoneNumber { Country = czechRepublic, Number = 123456789 }
+			};
+
+			rootDesc.Serialize(Console.Out, person);
+		}
+
+		static RootDescriptor<Person> GetPersonDescriptor()
+		{
+			var rootDesc = new RootDescriptor<Person>();
+
+			List<GetFieldName> getFieldNames = new List<GetFieldName>();
+			List<GetValue> getValues = new List<GetValue>();
+
+			PersonDescriptor personDescriptor = new PersonDescriptor();
+
+			rootDesc.fillValues = new FillValues<Person>(personDescriptor.FillValues);
+
+			return rootDesc;
+		}
+	}
+}
