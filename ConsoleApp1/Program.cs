fadc2066dc3d06466078f22744d180e4e1dde2e0
prusovak2 <prusova.k2@seznam.cz>
2020-04-21 16:29:45 +0200

    Code commented

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,40 +7,69 @@ using System.IO;
 
 namespace UnreflectedSerializer
 {
+	/// <summary>
+	/// delegate to function appending a string reprsentation of T instance to StringBuilder builder
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="instance"></param>
+	/// <param name="builder"></param>
 	public delegate void GetStringRepresentation<T>(T instance, StringBuilder builder);
-
+	/// <summary>
+	/// SERIALIZER
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public class RootDescriptor<T>
 	{
+		/// <summary>
+		/// array containig delegates to functions returnig a string representation of fields of object described by this descriptor
+		/// </summary>
 		public GetStringRepresentation<T>[] FuctionsDescribingThis;
-
+		/// <summary>
+		/// to construct desciptors simple types - int string...
+		/// empty delegate array indicates a simple type
+		/// </summary>
 		public RootDescriptor()
 		{
 			this.FuctionsDescribingThis = new GetStringRepresentation<T>[0];
 		}
+		/// <summary>
+		/// to construct desciptors for compound types
+		/// </summary>
 		public RootDescriptor(GetStringRepresentation<T>[] delegates)
 		{
 			this.FuctionsDescribingThis = delegates;
 		}
-
+		/// <summary>
+		/// wrapper around my own serialize, dealing with a StringBuilder
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="instance"></param>
 		public void Serialize(TextWriter writer, T instance)
 		{
 			StringBuilder builder = new StringBuilder();
 			Serialize(instance, typeof(T).Name, builder);
 			writer.Write(builder.ToString());
 		}
-
+		/// <summary>
+		/// fills a stringbuilder with a string representation of T instance
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <param name="instanceName"></param>
+		/// <param name="builder"></param>
 		public void Serialize(T instance, string instanceName, StringBuilder builder)
 		{
 			if (this.FuctionsDescribingThis.Length == 0)
 			{
+				//simple value
 				builder.Append($"<{instanceName}>{instance}</{instanceName}>\n");
 			}
 			else
 			{
+				//compund value
 				builder.Append($"<{instanceName}>\n");
 				foreach (var GetStringRep in this.FuctionsDescribingThis)
 				{
-					GetStringRep(instance, builder);
+					GetStringRep(instance, builder); //function hidden in FunctionDescribingThis call serialize() - hidden recursion
 				}
 				builder.Append($"</{instanceName}>\n");
 			}
@@ -94,9 +123,17 @@ namespace UnreflectedSerializer
 
 			rootDesc.Serialize(Console.Out, person);
 		}
+		//Follow a set of methods serving as a configuration for a serializer
 
+		/// <summary>
+		/// returns a description of Person 
+		/// </summary>
+		/// <returns></returns>
 		static RootDescriptor<Person> GetPersonDescriptor()
 		{
+			//Create delegates to functions describing Person fields
+			//functions are to be called from the Serialize function
+			//descritors for each field nested anywhere in Person object is recursivelly created and .Serialize() is called on each descriptor
 			GetStringRepresentation<Person> FirstName = new GetStringRepresentation<Person>((Person person, StringBuilder builder) =>
 				{  GetStringDescriptor().Serialize(person.FirstName, nameof(person.FirstName), builder); });
 			GetStringRepresentation<Person> LastName = new GetStringRepresentation<Person>((Person person, StringBuilder builder ) =>
@@ -110,12 +147,17 @@ namespace UnreflectedSerializer
 			GetStringRepresentation<Person> MobilePhone = new GetStringRepresentation<Person>((Person person, StringBuilder builder) =>
 				{  GetPhoneNumberDescriptor().Serialize(person.MobilePhone, nameof(person.MobilePhone), builder); });
 
+			//create new person descriptor with coresponding array of delegates
 			RootDescriptor<Person> rootDesc = new RootDescriptor<Person>(new GetStringRepresentation<Person>[]
 			{FirstName, LastName, HomeAddress, WorkAddress, CitizenOf, MobilePhone });
 			
 			return rootDesc;
+			//no Person instance is passed directly to GetDescriptor() functions - Descriptors can be created independentally of a particular instance
+			//Person instance is passed to Serialize function when called, then propageted thru corresponding delegates to recursive serialize calls
 		}
 
+		//Descriptors for simple types, string and int is currently supported
+		//to support any other simple type, add corresponding GetDescrtiptor method
 		static RootDescriptor<int> GetIntDescriptor()
 		{
 			return new RootDescriptor<int>(); //instance of root descriptor with an empty delegate list
